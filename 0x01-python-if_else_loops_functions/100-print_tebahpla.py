9400cc3da84d84ceed733635f1362c42fb1af4c6
Jasmine <jasmineenams@gmail.com>
2023-06-06 12:45:00 -0700

    tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for j in range(ord('z'), ord('a') -1, -1):
-    if j % 2 == 0:
+for i in range(ord('z'), ord('a') -1, -1):
+    if i % 2 == 0:
         diff = 0
     else:
         diff = 32
