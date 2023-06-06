dba7b209c0db5fd97b665c28c9736a0e3dbad3de
Jasmine <jasmineenams@gmail.com>
2023-06-06 09:25:31 -0700

    print comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for letter in range(0, 10):
-    for l in range(0, 10):
+    for le in range(0, 10):
         if letter >= le:
             continue
         elif letter >= 8 and le >= 9:
