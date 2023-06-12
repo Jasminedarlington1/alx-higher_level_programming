901b3ea4fbef25de020fd818acb5918254c99895
Jasmine <jasmineenams@gmail.com>
2023-06-12 06:48:08 -0700

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == '':
-        first_c = sentence[0]
-    else:
-        first_c = None
-        return (len(sentence), first_c)
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
