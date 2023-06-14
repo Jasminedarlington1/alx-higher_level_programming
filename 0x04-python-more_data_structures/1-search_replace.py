1dfdf644574d741d2e4442e7c141d04d4f817bb5
Jasmine <jasmineenams@gmail.com>
2023-06-14 07:34:19 -0700

    search and replace func

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return (list(map(lambda new_element: if new_element != search else replace, my_list)))
+    return (list(map(lambda new_element: new_element if new_element != search else replace, my_list)))
