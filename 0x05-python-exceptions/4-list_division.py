274852d76e2dccdfa8e9d14b52efaebe5048449c
Jasmine <jasmineenams@gmail.com>
2023-06-26 11:49:30 -0700

    list division

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for k in range(0, list_length):
+        try:
+            div = my_list_1[k] / my_list_2[k]
+        except TypeError:
+            print("wrong type")
+            div = 0
+        finally:
+            new_list.append(div)
+    return new_list
