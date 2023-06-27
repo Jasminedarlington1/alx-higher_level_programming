7b56c8e018b769b3f4442b838a1966230c1f495f
Jasmine <jasmineenams@gmail.com>
2023-06-27 10:35:05 -0700

    area square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Creates a square"""
+
+
+class Square:
+    """Not an empty square"""
+
+    def __init__(self, size=0):
+
+        """Initializing the class square with size attribute"""
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Square area"""
+        return (self.__size)**2
