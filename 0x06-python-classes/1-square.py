6b4553d9c41570d3e7a6a3eda68a38969211e267
Jasmine <jasmineenams@gmail.com>
2023-06-27 10:04:47 -0700

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Creates a square"""
+
+
+class Square:
+    """Not an empty square"""
+
+    def __init__(size):
+
+        """Initializing the class size
+           Args: size - size of the square created"""
+
+           self._size = size
