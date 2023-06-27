179b839c86a7ed930dc1535ee6a1173df53ff448
Jasmine <jasmineenams@gmail.com>
2023-06-27 10:53:30 -0700

    squares 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-
+"""Creates a square class"""
 
 class Square:
     """Square class"""
 
     def __init__(self, size=0):
         """
-        Initializing the square size
+        Initializing a new class
         """
-
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
 
         self.__size = size
 
@@ -24,7 +19,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if type(value)not int:
+        if type(value) is not int:
             raise TypeError('size must be an integer')
         if value < 0:
             raise ValueError('size must be >= 0')
