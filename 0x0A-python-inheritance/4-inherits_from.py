d7ce09e224afdbdc7c355be0b6b16cb24d0ac50f
Jasmine <jasmineenams@gmail.com>
2023-07-11 14:02:13 -0700

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""initializate module"""
+
+
+def inherits_from(obj, a_class):
+    """function that returns True if the object is an instance of a class
+     that inherited (directly or indirectly) from the specified class"""
+
+    return isinstance(obj, a_class) and type(obj) is not a_class
