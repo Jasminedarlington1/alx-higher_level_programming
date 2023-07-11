da0195e3c393104f063fb9aa2f1f12cac2fd7da3
Jasmine <jasmineenams@gmail.com>
2023-07-11 13:26:43 -0700

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+module for lookup.
+"""
+
+
+def lookup(obj):
+
+    """
+    the list of available attributes and
+    methods of an object
+    """
+
+    return dir(obj)
