2bea4b09d8ef47087362019056cf2180287de2ff
Jasmine <jasmineenams@gmail.com>
2023-09-12 10:25:00 -0700

    task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+exports.list = [1, 2, 3, 4, #!/usr/bin/node
+const list = require('./100-data').list;
+
+const newList = list.map((x, index) => x * index);
+
+console.log(list);
+console.log(newList);5];
